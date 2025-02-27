85224576a636c14aff65e6e2438bb6e5d15537da
EhrickAyrhand <lordayrhand@gmail.com>
2025-02-26 21:56:57 -0300

    Adicionado Print de quais itens foram removidos e seus respectiv os valores

--- Tests/remove_item_test.py
+++ Tests/remove_item_test.py
@@ -3,7 +3,6 @@ from Tests.shopping_test import adicionar_item_ao_carrinho
 import time
 
 def remover_itens_do_carrinho(browser):
-    
     adicionar_item_ao_carrinho(browser)
     
     browser.get("https://www.saucedemo.com/cart.html")
@@ -15,33 +14,41 @@ def remover_itens_do_carrinho(browser):
     for item in cart_items:
         price_element = item.find_element(By.CLASS_NAME, "inventory_item_price")
         price_text = price_element.text
-        inventory_item_name = item.find_element(By.CLASS_NAME, "inventory_item_name")
-        inventory_item_name_text = inventory_item_name.text
         price = float(price_text.replace("$", ""))
         
         if price > 15.99:
             remove_button = item.find_element(By.CLASS_NAME, "cart_button")
-            remove_buttons.append(remove_button)
+            remove_buttons.append((remove_button, item))
     
     if remove_buttons:
-        for button in remove_buttons:
+        for button, item in remove_buttons:
             time.sleep(2)
+            
+            inventory_item_name = item.find_element(By.CLASS_NAME, "inventory_item_name")
+            inventory_item_name_text = inventory_item_name.text
+            price_element = item.find_element(By.CLASS_NAME, "inventory_item_price")
+            price_text = price_element.text
+            
+            print(f"Item removido da lista: {inventory_item_name_text}. Com o valor de {price_text}")
+            
             button.click()
     else:
         print("Nenhum item com preço acima de 15.99 encontrado no carrinho.")
     
     total_price = 0.0
-
     remaining_items = browser.find_elements(By.CLASS_NAME, "cart_item")
+    
     for item in remaining_items:
         price_element = item.find_element(By.CLASS_NAME, "inventory_item_price")
         price_text = price_element.text
+        price = float(price_text.replace("$", ""))
+        
         inventory_item_name = item.find_element(By.CLASS_NAME, "inventory_item_name")
         inventory_item_name_text = inventory_item_name.text
-        price = float(price_text.replace("$", ""))
+        
         assert price <= 15.99, f"Item com preço {price} ainda está no carrinho."
-        print(f"no carrinho há os seguintes: {inventory_item_name_text} valores de itens {price}")
-
+        print(f"No carrinho há os seguintes: {inventory_item_name_text}. Valor dos itens: {price}")
+        
         total_price += price
-
+    
     print(f"Valor final é {total_price}")
